65a909d14a716090f2ebd1e439b5b222a22db648
harshBhai <harshgoel18122001@gmail.com>
2024-02-09 22:52:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({firstName:"", lastName:"", email:""})
+
+  function changeHandler(event){
+    const {name, value, type, checked} = event.target
+    setFormData(prevFromData=>{
+      return {
+        ...prevFromData,
+        [name]: type === "checkbox" ? checked : value 
+      }
+    })
+  }
+
+  function submitHandler(event){
+    event.preventDefault();
+    console.log(formData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen flex flex-col items-center mt-4'>
+      <div className='w-[400px] border'>
+        <label>First Name</label>
+        <br></br>
+        <input
+          className='mt-1'
+          placeholder='Harsh'
+          type='text'
+          name='firstName'
+          value={formData.firstName}
+        />
+        <br/>
+        <label>Last Name</label>
+        <br></br>
+        <input
+          className='mt-1'
+          placeholder='Goel'
+          type='text'
+          name='lastName'
+          value={formData.lastName}
+        />
+        <br/>
+        <label>Email</label>
+        <br/>
+        <input
+          className='mt-1'
+          placeholder='harshgoel4@gmai.com'
+          type='email'
+          name='email'
+          value={formData.email}
+        />
+
+      </div>
     </div>
   );
 }
